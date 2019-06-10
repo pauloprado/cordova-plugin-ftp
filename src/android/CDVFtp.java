b7646b7052ea89d295c3591c5c398724373a864b
Paulo Prado <paulo@phelcom.com.br>
2019-06-10 17:10:04 -0300

    fix(ftps): add custom SSlSocketFactory that doesnt perform any validation

--- src/android/CDVFtp.java
+++ src/android/CDVFtp.java
@@ -39,6 +39,16 @@ import it.sauronsoftware.ftp4j.FTPClient;
 import it.sauronsoftware.ftp4j.FTPDataTransferListener;
 import it.sauronsoftware.ftp4j.FTPFile;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+
 public class CDVFtp extends CordovaPlugin {
     public static final String TAG = CDVFtp.class.getSimpleName();
     private String rootPath = "/";
@@ -168,8 +178,29 @@ public class CDVFtp extends CordovaPlugin {
             }
 
             try {
+
+                TrustManager[] trustManager = new TrustManager[] { new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                } };
+                SSLContext sslContext = null;
+                try {
+                    sslContext = SSLContext.getInstance("SSL");
+                    sslContext.init(null, trustManager, new SecureRandom());
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (KeyManagementException e) {
+                    e.printStackTrace();
+                }
+                SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
                 this.client = new FTPClient();
                 // if(ftps){
+                    this.client.setSSLSocketFactory(sslSocketFactory);
                     this.client.setSecurity(FTPClient.SECURITY_FTPS);
                 // }
                 String[] address = hostname.split(":");
